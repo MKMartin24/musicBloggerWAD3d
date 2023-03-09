0f668110b50ce310bcd2b378e49b1154faa39986
“Isaac <isaac.a.gilbert@gmail.com>
2023-03-09 16:11:02 +0000

    add unittest for models

--- musicBlogger/models.py
+++ musicBlogger/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Songs (models.Model):
+    name = models.CharField(max_length=128, default="No-Name")
     spotifyURL = models.CharField(max_length=128)
     youtubeURL = models.CharField(max_length=128)
     description = models.CharField(max_length=512)
@@ -11,7 +12,6 @@ class Songs (models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.CharField(max_length=30, unique=True)
     picture = models.ImageField(upload_to='profile_images', blank=True)
     artist = models.BooleanField()
     review = models.BooleanField()
@@ -22,21 +22,13 @@ class UserProfile(models.Model):
         return self.user.username
     
 
-# class UserRelationship(models.Model):
-#     types = models.ManyToManyField('RelationshipType', blank=True,
-#                                    related_name='user_relationships')
-#     from_contact = models.ForeignKey('User', related_name='from_user')
-#     to_contact = models.ForeignKey('User', related_name='to_user')
-#     class Meta:
-#         unique_together = ('from_user', 'to_user')
-
 
 class Blogs(models.Model):
     title = models.CharField(max_length=128)
     date = models.DateField()
-    image = models.ImageField(upload_to='blog_images')
+    image = models.ImageField(upload_to='blog_images', blank=True)
     text = models.CharField(max_length=4096)
-    postedBy = models.ManyToManyField(UserProfile)
+    postedBy = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
     def __str__(self):
         return self.title
 
@@ -44,6 +36,6 @@ class Comments(models.Model):
     content = models.CharField(max_length=1000)
     date = models.DateField()
     blog = models.ForeignKey(Blogs, on_delete = models.CASCADE)
-    commentedBy = models.ManyToManyField(UserProfile)
+    commentedBy = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
     def __str__(self):
         return self.content
