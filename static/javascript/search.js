889558aec754972ef57e2fd50a1844ef9d037661
“Isaac <isaac.a.gilbert@gmail.com>
2023-03-12 21:50:10 +0000

    add basic search js

---
+++ static/javascript/search.js
@@ -0,0 +1,26 @@
+function search() {
+    var query = document.getElementById('search-input').value;
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var data = JSON.parse(xhr.responseText);
+            showResults(data.results);
+        }
+    };
+    xhr.open('GET', '/search?q=' + query);
+    xhr.send();
+}
+
+function showResults(results) {
+    var output = '';
+    if (results.length > 0) {
+        output += '<ul>';
+        for (var i = 0; i < results.length; i++) {
+            output += '<li>' + results[i].field1 + ' - ' + results[i].field2 + '</li>';
+        }
+        output += '</ul>';
+    } else {
+        output += 'No results found.';
+    }
+    document.getElementById('search-results').innerHTML = output;
+}
