31160ec6e5c68728eb3d06e7d9c3f65d60f1d209
Mairi Martin <mairi.kate0815@gmail.com>
2023-03-14 19:18:48 +0000

    Group meeting - links mostly working now

--- musicBlogger/views.py
+++ musicBlogger/views.py
@@ -26,7 +26,7 @@ def login(request):
 def search(request):
     context_dict = {}
     context_dict['message'] ="This is the search page"
-    return render(request, 'musicBlogger/search.html', context=context_dict)
+    return render(request, 'musicBlogger/searchBlogs.html', context=context_dict)
 
 def add_blog(request, blog_name_slug):
     context_dict = {}
@@ -37,3 +37,23 @@ def view_blog(request):
     context_dict = {}
     context_dict['message'] ="This is the view blog page"
     return render(request, 'musicBlogger/viewBlog.html', context=context_dict)
+
+def contact_us(request):
+    context_dict = {}
+    context_dict['message'] ="This is the contact us page"
+    return render(request, 'musicBlogger/contact_us.html', context=context_dict)
+
+def profile(request):
+    context_dict = {}
+    context_dict['message'] ="This is the profile page"
+    return render(request, 'musicBlogger/profile.html', context=context_dict)
+
+def login(request):
+    context_dict = {}
+    context_dict['message'] ="This is the login page"
+    return render(request, 'musicBlogger/login.html', context=context_dict)
+
+def new_account(request):
+    context_dict = {}
+    context_dict['message'] ="This is the new account page"
+    return render(request, 'musicBlogger/new_account.html', context=context_dict)
