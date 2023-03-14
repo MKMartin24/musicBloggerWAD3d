4ab5ca832e03af2ea58494084b87d78a78830431
Echizen <harry.chiu.hc@gmail.com>
2023-03-14 17:24:51 +0000

    Added some views

--- musicBlogger/urls.py
+++ musicBlogger/urls.py
@@ -6,4 +6,8 @@ app_name = "music blogger"
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/',views.about,name='about'),
+    path('login/', views.login, name='login'),
+    path('profile/', views.profile, name='profile'),
+    path('search/', views.search, name='search'),
+
 ]
--- musicBlogger/views.py
+++ musicBlogger/views.py
@@ -6,3 +6,23 @@ from django.shortcuts import redirect
 def index(request):
     context_dict = "this is the index page"
     return render(request, 'musicBlogger/index.html', context=context_dict)
+
+def about(request):
+    context_dict = "This is the about page"
+    return render(request, 'musicBlogger/about.html', context=context_dict)
+
+def login(request):
+    context_dict = "This is the login page"
+    return render(request, 'musicBlogger/login.html', context=context_dict)
+
+def search(request):
+    context_dict = "This is the search page"
+    return render(request, 'musicBlogger/search.html', context=context_dict)
+
+def add_blog(request, blog_name_slug):
+    context_dict = "This is the add blog page"
+    return render(request, 'musicBlogger/add_blog.html', context=context_dict)
+
+def view_blog(request):
+    context_dict = "This is the view blog page"
+    return render(request, 'musicBlogger/viewBlog.html', context=context_dict)
