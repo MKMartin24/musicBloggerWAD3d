b6c24a5f334b67c928ade3c0994e8367f7f0be91
Mairi Martin <mairi.kate0815@gmail.com>
2023-03-14 17:47:55 +0000

    Website should load now

--- musicBlogger/views.py
+++ musicBlogger/views.py
@@ -4,5 +4,9 @@ from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
-    context_dict = "this is the index page"
+    context_dict = {"text":"this is the index page"}
     return render(request, 'musicBlogger/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {}
+    return render(request, 'musicBlogger/about.html',context=context_dict)
