88185f6a53c4b4c75d18a67937229cf20479891b
Mairi Martin <mairi.kate0815@gmail.com>
2023-03-18 12:53:20 +0000

    added logout url and link

--- musicBlogger/admin.py
+++ musicBlogger/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from musicBlogger.models import Artist,Songs,UserProfile,Blogs
 # Register your models here.
+
+admin.site.register(Artist)
+admin.site.register(Songs)
+admin.site.register(UserProfile)
+admin.site.register(Blogs)
