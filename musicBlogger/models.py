20fa6d9f02fedc6e47c4200c803b76f68d4bcc82
Jay Yuen <2675647Y@student.gla.ac.uk>
2023-03-19 00:48:31 +0000

    Completed populate script, Update models

--- musicBlogger/models.py
+++ musicBlogger/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 
 class Artist(models.Model):
     artistName = models.CharField(max_length=128)
+    def __str__(self):
+        return self.artistName
 
 class Songs (models.Model):
     name = models.CharField(max_length=128, default="No-Name")
@@ -15,34 +17,41 @@ class Songs (models.Model):
     image = models.ImageField(upload_to='cover_images')
     genre = models.CharField(max_length=128)
     madeBy = models.ForeignKey(Artist, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.name
 
     
 class UserProfile(models.Model):
-    name = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     text = models.CharField(max_length=1000)
     image = models.ImageField(upload_to='profile_images', blank=True)
-    likedSong = models.ManyToManyField(Songs)
+    likedSong = models.ManyToManyField(Songs, related_name='liked_song')
     artist = models.ManyToManyField(Artist)
     follows = models.ManyToManyField('self')
+
+    # list of follows_user's id
+    # [2, 3, 4]
+    # follows = models.JsonField()
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
+        self.slug = slugify(self.user.username)
         super(UserProfile, self).save(*args, **kwargs)
     def __str__(self):
         return self.user.username
 
 
 class Blogs(models.Model):
-    name = models.CharField(max_length=128)
-    date = models.DateField()
+    title = models.CharField(max_length=128)
+    date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='blog_images', blank=True)
     text = models.CharField(max_length=4096)
     postedBy = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
     def __str__(self):
         return self.title
 
+
 class Comments(models.Model):
     content = models.CharField(max_length=1000)
-    date = models.DateField()
+    date = models.DateTimeField(auto_now_add=True)
     blog = models.ForeignKey(Blogs, on_delete = models.CASCADE)
     commentedBy = models.ForeignKey(UserProfile, on_delete = models.CASCADE)
     def __str__(self):
